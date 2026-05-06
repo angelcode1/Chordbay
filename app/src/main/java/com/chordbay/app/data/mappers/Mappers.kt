8323bf3f56b5ddec18eae4d446597d3189421608
angelcode1 <wildchild.313@gmail.com>
2026-05-07 09:57:30 +1000

    Update Mappers.kt

--- app/src/main/java/com/chordbay/app/data/mappers/Mappers.kt
+++ app/src/main/java/com/chordbay/app/data/mappers/Mappers.kt
@@ -1,43 +1,14 @@
 package com.chordbay.app.data.mappers
 
-import com.chordbay.app.data.database.song.SongEntity
 import com.chordbay.app.data.model.Song
 import com.chordbay.app.data.model.chord.HBFormat
 import com.chordbay.app.data.remote.model.SongDto
 
-fun SongEntity.toSong(): Song =
-    Song(
-        localId = id,
-        remoteId = remoteId,
-        title = title,
-        artist = artist,
-        content = content,
-        hBFormat = hBFormat
-    )
 fun SongDto.toSong(): Song =
     Song(
-        remoteId = id,
+        remoteId = id ?: sourceUrl, // Use URL as a unique ID for search results
         title = title,
         artist = artist,
         content = content,
-        isPublic = isPublic,
         hBFormat = if(germanNotation) HBFormat.GER else HBFormat.ENG
-    )
-fun Song.toDto(isRequest: Boolean = true): SongDto =
-    SongDto(
-        id = if (isRequest) null else remoteId,
-        title = title,
-        artist = artist,
-        content = content,
-        isPublic = isPublic,
-        germanNotation = hBFormat == HBFormat.GER
-    )
-fun Song.toSongEntity(): SongEntity =
-    SongEntity(
-        id = localId ?: 0,
-        remoteId = remoteId,
-        title = title,
-        artist = artist,
-        content = content,
-        hBFormat = hBFormat
     )
